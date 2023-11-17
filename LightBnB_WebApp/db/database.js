e6dd0b0071a22fe98b7b80b913c7076d6ed5233b
Ana <anmeuri@gmail.com>
2023-11-16 22:31:13 -0800

    Refactor function to add new listings - Insert into DB

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -192,10 +192,40 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *`;
+
+  const owner_id = property.owner_id;
+  const title = property.title;
+  const description = property.description;
+  const thumbnail_photo_url = property.thumbnail_photo_url;
+  const cover_photo_url = property.cover_photo_url;
+  const cost_per_night = property.cost_per_night;
+  const street = property.street;
+  const city = property.city;
+  const province = property.province;
+  const post_code = property.post_code;
+  const country = property.country;
+  const parking_spaces = property.parking_spaces;
+  const number_of_bathrooms = property.number_of_bathrooms;
+  const number_of_bedrooms = property.number_of_bedrooms
+
+  return pool
+    .query(queryString, [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log('error:', err.message);
+    });
+
 };
 
 module.exports = {
